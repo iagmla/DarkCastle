1a2f165819b67795052c2297c7468a27a52b98cd
Karl Zander <karl.zander.medicated@gmail.com>
2024-02-23 17:29:03 -0800

    bugs full of bugs

--- example/example.sh
+++ example/example.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-castle zanderfish3 -e msg test.enc castle_test.pk castle_hello.sk && castle zanderfish3 -d test.enc test.dec castle_test.sk castle_test.pk
+castle qapla -e msg test.enc castle_test.pk castle_hello.sk && castle qapla -d test.enc test.dec castle_test.sk castle_test.pk
